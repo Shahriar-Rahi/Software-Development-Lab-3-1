0f28e26715376b25a3f780f1746d8e835ed6ff48
nabid22 <30776415+nabid22@users.noreply.github.com>
2018-08-07 20:52:19 +0600

    index file created which is the server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db.js');
+var departmentController = require('./controllers/departmentController.js');
+var facultyController = require('./controllers/facultyController.js');
+
+var app = express();
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log('Server started at port : 3000'));
+
+app.use('/departments', departmentController);
+app.use('/faculties', facultyController);
