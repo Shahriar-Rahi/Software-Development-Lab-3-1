4e135dc8a11ec2b9754015b4fe8858445b4d165f
Shahriar Rahi <md.alshahriarrahi@gmail.com>
2018-08-08 00:31:55 +0600

    after adding info collection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { mongoose } = require('./db.js');
 var departmentController = require('./controllers/departmentController.js');
 var facultyController = require('./controllers/facultyController.js');
+var infoController = require('/controllers/infoController.js');
 
 var app = express();
 app.use(bodyParser.json());
@@ -14,3 +15,4 @@ app.listen(port, () => console.log('Server started at port...'));
 
 app.use('/departments', departmentController);
 app.use('/faculties', facultyController);
+app.use('/infos', infoController);
